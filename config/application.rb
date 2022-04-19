8c2cf3f14aba8c96eebf5c22a2f60071763986f7
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-19 10:28:05 -0500

    Add Sidkiq

--- config/application.rb
+++ config/application.rb
@@ -31,3 +31,7 @@ module DailyMood
     config.generators.system_tests = nil
   end
 end
+
+config.session_store :cookie_store, key: '_interslice_session'
+config.middleware.use ActionDispatch::Cookies
+config.middleware.use config.session_store, config.session_options
